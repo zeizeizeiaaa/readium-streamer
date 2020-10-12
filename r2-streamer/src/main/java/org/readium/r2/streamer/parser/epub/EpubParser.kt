9e9fecbfe468340708f628fe25ab5d9d18f1a823
Bryan <531318683@qq.com>
2020-10-12 13:31:59 +0800

    e-book

--- r2-streamer/src/main/java/org/readium/r2/streamer/parser/epub/EpubParser.kt
+++ r2-streamer/src/main/java/org/readium/r2/streamer/parser/epub/EpubParser.kt
@@ -186,8 +186,9 @@ class EpubParser : PublicationParser {
 
     private fun parseNavigationData(packageDocument: PackageDocument, container: Container): Map<String, List<Link>> =
         if (packageDocument.epubVersion < 3.0) {
+            //MediaType.NCX.contains(it.mediaType)
             val ncxItem = packageDocument.manifest.firstOrNull {
-                MediaType.NCX.contains(it.mediaType) || MediaType.XHTML.contains(it.mediaType)
+                "ncx".contains(it.id!!)
             }
             ncxItem?.let {
                 val ncxPath = normalize(packageDocument.path, ncxItem.href)
